5336790641b8fabcbe6a014b9d3c52130396d60b
Adrienne Li <adrienneli100@gmail.com>
2023-06-13 15:03:53 -0400

    Test

--- views/submit.js
+++ views/submit.js
@@ -16,38 +16,6 @@ document.getElementById("submitBtn").addEventListener("click", function(event){
   let fileName = file.name;
   let ext = fileName.split('.')[1];
   let folderName = document.getElementById("folder").value;
-  console.log("BEFORE IF");
-  console.log(ext);
-  if(ext == "xlsx"){
-    console.log("HIHI");
-    var XLSX = require('xlsx');
-    var workbook = XLSX.readFile(fileName);
-    var sheet_name_list = workbook.SheetNames;
-    for (let i = 0; i < sheet_name_list.length; i++) {
-      
-      console.log(sheet_name_list[i]);
-    }
-    // const xlsxFile = require('read-excel-file/node');
-    //   xlsxFile('./Data.xlsx').then((rows) => {
-    //     console.log(rows);
-    //     console.table(rows);
-    //   })
-  }
-  else{
-    console.log("ESLE");
-  }
-
-
-
-
-
-
-
-
-
-
-
-
   // Create new file so we can rename the file
   let blob = file.slice(0, file.size, ext);
   newFile = new File([blob], `${postid}_post.${ext}`, { type: ext });
